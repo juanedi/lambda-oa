9b01eb965ea37350d0021324550866442cd969a4
Juan Edi <juanedi@zaubersoftware.com>
2013-08-23 12:57:01 -0300

    Frag now has a timestamp and game name

--- batch/service/src/main/java/com/zauberlabs/bigdata/lamdaoa/batch/Frag.java
+++ batch/service/src/main/java/com/zauberlabs/bigdata/lamdaoa/batch/Frag.java
@@ -3,6 +3,8 @@
  */
 package com.zauberlabs.bigdata.lamdaoa.batch;
 
+import java.util.Date;
+
 /**
  * Frag event.
  * 
@@ -11,8 +13,25 @@ package com.zauberlabs.bigdata.lamdaoa.batch;
  */
 public class Frag {
 
+    private Date timeStamp;
+    private String game;
     private String fragger;
     private String fragged;
+    
+    public Frag(Date timeStamp, String game, String fragger, String fragged) {
+        this.timeStamp = timeStamp;
+        this.game = game;
+        this.fragger = fragger;
+        this.fragged = fragged;
+    }
+
+    public Date getTimestamp() {
+        return timeStamp;
+    }
+    
+    public String getGame() {
+        return game;
+    }
     
     public String getFragger() {
         return fragger;
